9c2371d9dc03d79e5313648c7fb99049ad43dc32
MaTachi <dannie92@gmail.com>
2014-04-22 15:52:50 +0200

    Add STATIC_ROOT setting

--- hydra/settings.py
+++ hydra/settings.py
@@ -84,6 +84,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'blog/templates'),
     os.path.join(BASE_DIR, 'hydra/templates'),
