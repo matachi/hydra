e873d12e1e83bb70e145a39dcebe91654358d4bc
MaTachi <dannie92@gmail.com>
2014-04-23 12:11:41 +0200

    Add an admin action to re-save posts

--- blog/admin.py
+++ blog/admin.py
@@ -13,9 +13,16 @@ class TagInline(admin.TabularInline):
     extra = 1
 
 
+def resave(modeladmin, request, queryset):
+    for post in queryset:
+        post.save()
+resave.short_description = 'Update selected posts\' HTML and slug'
+
+
 class PostAdmin(admin.ModelAdmin):
     # inlines = TagInline,
     readonly_fields = 'content_html', 'slug'
+    actions = [resave]
 
 admin.site.register(Post, PostAdmin)
 
